83f64a826332ab2ca93a6ea0123627dfa87c6ef2
KhushiS14 <131450268+KhushiS14@users.noreply.github.com>
2023-06-23 14:18:38 +0530

    ch 1

---
+++ ch 1/ch1.1.cpp
@@ -0,0 +1,201 @@
+#include<iostream>
+using namespace std;
+class employ
+{
+	public:
+	int emp_id;
+	char emp_name[100];
+	int emp_role;
+	int emp_age;
+	int emp_salary;
+	int emp_experience;
+	char emp_city[100];
+	char emp_company_name[100];
+};
+
+int main()
+{
+	employ s1;
+	
+	s1.emp_id;
+	s1.emp_name;
+	s1.emp_role;
+	s1.emp_age;
+	s1.emp_salary;
+	s1.emp_experience;
+	s1.emp_city;
+	s1.emp_company_name;
+	
+	cout<<"emp_id : ";
+	cin>>s1.emp_id;
+	
+	cout<<"emp_name : ";
+	cin>>s1.emp_name;
+	
+	cout<<"emp_role : ";
+	cin>>s1.emp_role;
+	
+	cout<<"emp_age : ";
+	cin>>s1.emp_age;
+	
+	cout<<"emp_salary : ";
+	cin>>s1.emp_salary;
+	
+	cout<<"emp_experience : ";
+	cin>>s1.emp_experience;
+	
+	cout<<"emp_city : ";
+	cin>>s1.emp_city;
+	
+	cout<<"emp_company_name : ";
+	cin>>s1.emp_company_name;
+	
+	printf("\n\n-----------------\n\n");
+	
+	employ s2;
+	
+	s2.emp_id;
+	s2.emp_name;
+	s2.emp_role;
+	s2.emp_age;
+	s2.emp_salary;
+	s2.emp_experience;
+	s2.emp_city;
+	s2.emp_company_name;
+	
+	cout<<"emp_id : ";
+	cin>>s2.emp_id;
+	
+	cout<<"emp_name : ";
+	cin>>s2.emp_name;
+	
+	cout<<"emp_role : ";
+	cin>>s2.emp_role;
+	
+	cout<<"emp_age : ";
+	cin>>s2.emp_age;
+	
+	cout<<"emp_salary : ";
+	cin>>s2.emp_salary;
+	
+	cout<<"emp_experience : ";
+	cin>>s2.emp_experience;
+	
+	cout<<"emp_city : ";
+	cin>>s2.emp_city;
+	
+	cout<<"emp_company_name : ";
+	cin>>s2.emp_company_name;
+	
+	printf("\n\n-----------------\n\n");
+	
+	employ s3;
+	
+	s3.emp_id;
+	s3.emp_name;
+	s3.emp_role;
+	s3.emp_age;
+	s3.emp_salary;
+	s3.emp_experience;
+	s3.emp_city;
+	s3.emp_company_name;
+	
+	cout<<"emp_id : ";
+	cin>>s3.emp_id;
+	
+	cout<<"emp_name : ";
+	cin>>s3.emp_name;
+	
+	cout<<"emp_role : ";
+	cin>>s3.emp_role;
+	
+	cout<<"emp_age : ";
+	cin>>s3.emp_age;
+	
+	cout<<"emp_salary : ";
+	cin>>s3.emp_salary;
+	
+	cout<<"emp_experience : ";
+	cin>>s3.emp_experience;
+	
+	cout<<"emp_city : ";
+	cin>>s3.emp_city;
+	
+	cout<<"emp_company_name : ";
+	cin>>s3.emp_company_name;
+	
+	
+	printf("\n\n-----------------\n\n");
+	
+	employ s4;
+	
+	s4.emp_id;
+	s4.emp_name;
+	s4.emp_role;
+	s4.emp_age;
+	s4.emp_salary;
+	s4.emp_experience;
+	s4.emp_city;
+	s4.emp_company_name;
+	
+	cout<<"emp_id : ";
+	cin>>s4.emp_id;
+	
+	cout<<"emp_name : ";
+	cin>>s4.emp_name;
+	
+	cout<<"emp_role : ";
+	cin>>s4.emp_role;
+	
+	cout<<"emp_age : ";
+	cin>>s4.emp_age;
+	
+	cout<<"emp_salary : ";
+	cin>>s4.emp_salary;
+	
+	cout<<"emp_experience : ";
+	cin>>s4.emp_experience;
+	
+	cout<<"emp_city : ";
+	cin>>s4.emp_city;
+	
+	cout<<"emp_company_name : ";
+	cin>>s4.emp_company_name;
+	
+	printf("\n\n-----------------\n\n");
+	
+	employ s5;
+	
+	s5.emp_id;
+	s5.emp_name;
+	s5.emp_role;
+	s5.emp_age;
+	s5.emp_salary;
+	s5.emp_experience;
+	s5.emp_city;
+	s5.emp_company_name;
+	
+	cout<<"emp_id : ";
+	cin>>s5.emp_id;
+	
+	cout<<"emp_name : ";
+	cin>>s5.emp_name;
+	
+	cout<<"emp_role : ";
+	cin>>s5.emp_role;
+	
+	cout<<"emp_age : ";
+	cin>>s5.emp_age;
+	
+	cout<<"emp_salary : ";
+	cin>>s5.emp_salary;
+	
+	cout<<"emp_experience : ";
+	cin>>s5.emp_experience;
+	
+	cout<<"emp_city : ";
+	cin>>s5.emp_city;
+	
+	cout<<"emp_company_name : ";
+	cin>>s5.emp_company_name;
+}
---
+++ ch 1/ch1.2.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+class car
+{
+	public:
+	int car_id;
+	char car_company_name[100];
+	char car_color[100];
+	int car_model;
+	int car_release_year;	
+};
+
+int main()
+{
+	
+	car c1;
+	
+	c1.car_id;
+	c1.car_company_name;
+	c1.car_color;
+	c1.car_model;
+	c1.car_release_year;
+	
+	cout<<"car_id : ";
+	cin>>c1.car_id;
+	
+	cout<<"car_company_name : ";
+	cin>>c1.car_company_name;
+	
+	cout<<"car_color : ";
+	cin>>c1.car_color;
+	
+	cout<<"car_model : ";
+	cin>>c1.car_model;
+	
+	cout<<"car_release_year : ";
+	cin>>c1.car_release_year;
+	
+	printf("\n\n-------------\n\n");
+	
+	car c2;
+	
+	c2.car_id;
+	c2.car_company_name;
+	c2.car_color;
+	c2.car_model;
+	c2.car_release_year;
+	
+	cout<<"car_id : ";
+	cin>>c2.car_id;
+	
+	cout<<"car_company_name : ";
+	cin>>c2.car_company_name;
+	
+	cout<<"car_color : ";
+	cin>>c2.car_color;
+	
+	cout<<"car_model : ";
+	cin>>c2.car_model;
+	
+	cout<<"car_release_year : ";
+	cin>>c2.car_release_year;
+	
+	printf("\n\n-------------\n\n");
+	
+	car c3;
+	
+	c3.car_id;
+	c3.car_company_name;
+	c3.car_color;
+	c3.car_model;
+	c3.car_release_year;
+	
+	cout<<"car_id : ";
+	cin>>c3.car_id;
+	
+	cout<<"car_company_name : ";
+	cin>>c3.car_company_name;
+	
+	cout<<"car_color : ";
+	cin>>c3.car_color;
+	
+	cout<<"car_model : ";
+	cin>>c3.car_model;
+	
+	cout<<"car_release_year : ";
+	cin>>c3.car_release_year;
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+}
